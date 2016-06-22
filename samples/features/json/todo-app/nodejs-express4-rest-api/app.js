e5b78ee6c0b074cade317c612993ec6c01b22de1
Jovan Popovic <jocapc@gmail.com>
2016-06-22 13:14:40 +0200

    Node.js REST APi
    Draft version of NodeJS REST API that uses SQL/JSON functions

---
+++ samples/features/json/todo-app/nodejs-express4-rest-api/app.js
@@ -0,0 +1,15 @@
+﻿var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.text());
+app.use('/todo', require('./routes/todo'));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+module.exports = app;
