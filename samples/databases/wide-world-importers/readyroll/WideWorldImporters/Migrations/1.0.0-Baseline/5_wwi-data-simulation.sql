c0b7cc4bdaa455c6b7ec0aa5a1dd04ead4afdcff
Daniel Nolan <daniel@ready-roll.com>
2016-07-20 13:31:15 +1000

    Don't run the data simulation against the Shadow DB
    It's unnecessary to run this ~15 minute operation against the Shadow, as the changes aren't picked up by the Compare engine anyway.

--- samples/databases/wide-world-importers/readyroll/WideWorldImporters/Migrations/1.0.0-Baseline/5_wwi-data-simulation.sql
+++ samples/databases/wide-world-importers/readyroll/WideWorldImporters/Migrations/1.0.0-Baseline/5_wwi-data-simulation.sql
@@ -6,21 +6,24 @@ GO
 USE [$(DatabaseName)];
 GO
 
-SET NOCOUNT ON;
+IF NOT DB_NAME() LIKE '%_SHADOW'
+BEGIN
+	SET NOCOUNT ON;
 
-EXEC DataLoadSimulation.Configuration_ApplyDataLoadSimulationProcedures;
+	EXEC DataLoadSimulation.Configuration_ApplyDataLoadSimulationProcedures;
 
-EXEC DataLoadSimulation.DailyProcessToCreateHistory 
-	@StartDate = '20130101',
-	@EndDate = '20151219', -- the day after 20151220 will trigger RecordColdRoomTemperatures, which is the next work item to tune
-	@AverageNumberOfCustomerOrdersPerDay = 60,
-	@SaturdayPercentageOfNormalWorkDay = 50,
-	@SundayPercentageOfNormalWorkDay = 0,
-	@UpdateCustomFields = 1,
-	@IsSilentMode = 1,
-	@AreDatesPrinted = 1,
-	@CommitBatchSize=10;
+	EXEC DataLoadSimulation.DailyProcessToCreateHistory 
+		@StartDate = '20130101',
+		@EndDate = '20151219', -- the day after 20151220 will trigger RecordColdRoomTemperatures, which is the next work item to tune
+		@AverageNumberOfCustomerOrdersPerDay = 60,
+		@SaturdayPercentageOfNormalWorkDay = 50,
+		@SundayPercentageOfNormalWorkDay = 0,
+		@UpdateCustomFields = 1,
+		@IsSilentMode = 1,
+		@AreDatesPrinted = 1,
+		@CommitBatchSize=10;
 
-EXEC WideWorldImporters.DataLoadSimulation.Configuration_RemoveDataLoadSimulationProcedures;
+	EXEC DataLoadSimulation.Configuration_RemoveDataLoadSimulationProcedures;
+END
 GO
 
